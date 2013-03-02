d3a24279d22f009a89926f8aa21727de8be697f8
Mark Gallop <mark@rollick.id.au>
2013-03-02 16:59:58 +0700

    fixes for foundation 4 refactor

--- client/js/main.js
+++ client/js/main.js
@@ -5,7 +5,7 @@ Meteor.subscribe("allGroups");
 Meteor.subscribe("directory");
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.8");
+  Session.set("appVersion", "v0.9.9");
 
   Backbone.history.start({ pushState: true });
 
