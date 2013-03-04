d86f9f3d819988d3fdba0b916d5526e96f775408
Mark Gallop <mark@rollick.id.au>
2013-03-04 21:56:03 +0700

    commit missing file changes

---
+++ client/js/views/nav.js
@@ -0,0 +1,34 @@
+Template.mainNav.rendered = function () {
+  // Init plugins
+  $(document).foundation("topbar", function (response) {
+    console.log(response.errors);
+  });
+};
+
+Template.mainNav.group = function () {
+  var group = getCurrentGroup();
+  if (group) {
+    return group;
+  } else if (Session.get("groupSlug")) {
+    group = Groups.findOne({slug: Session.get("groupSlug")});
+    if (!group) { // group hasn't loaded!
+      return false;
+    } else {
+      Session.set("groupId", group._id);
+      return Session.get("groupId");      
+    }
+  } else {
+    return false;
+  }
+};
+
+Template.mainNav.events({
+  'click .home': function () {
+    Router.setGroup(getCurrentGroup());
+    return false;
+  },
+  'click .main-home': function () {
+    Router.setHome();
+    return false;
+  }
+});
