fe17ba9ef251f88be7b394a0291666dfe8c478e7
Mark Gallop <mark@rollick.id.au>
2013-03-11 15:20:12 +0700

    use transparent svg's and some other style changes

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -200,7 +200,7 @@ var geoLocation = function(location, callback) {
 };
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.33");
+  Session.set("appVersion", "v0.9.34");
 
   Backbone.history.start({ pushState: true });
   // initTemplateChecks();
