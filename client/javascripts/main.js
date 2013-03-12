3f81cc8e0e3e05d7984368357037cea6de57ae43
Mark Gallop <mark@rollick.id.au>
2013-03-12 18:25:20 +0700

    fixes for firefox issues

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -200,7 +200,7 @@ var geoLocation = function(location, callback) {
 };
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.35");
+  Session.set("appVersion", "v0.9.36");
 
   Backbone.history.start({ pushState: true });
   // initTemplateChecks();
