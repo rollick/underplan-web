f9c93c83c3838ddf25325b0bc9354bbb23020c57
Mark Gallop <mark@rollick.id.au>
2013-03-15 14:51:20 +0700

    tweaking css for short comments

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -213,7 +213,7 @@ var trackRoute = function(action) {
 var timeout = null;
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.41");
+  Session.set("appVersion", "v0.9.42");
 
   Backbone.history.start({ pushState: true });
   // initTemplateChecks();
