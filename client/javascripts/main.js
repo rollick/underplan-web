22690263adab999de9b4c2b548bf45a38bba8d71
Mark Gallop <mark@rollick.id.au>
2013-03-17 04:28:04 +0700

    fix for mobile previous / next

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -205,7 +205,7 @@ var geoLocation = function(location, callback) {
 var timeout = null;
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.43");
+  Session.set("appVersion", "v0.9.44");
 
   Backbone.history.start({ pushState: true });
   // initTemplateChecks();
