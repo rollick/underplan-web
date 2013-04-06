0b7fee457d1174768a953645405f6f1dbc87b993
Mark Gallop <mark@rollick.id.au>
2013-04-06 13:12:30 +0700

    bigger remove links for comments

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -260,7 +260,7 @@ var geoLocation = function(location, callback) {
 var timeout = null;
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.62");
+  Session.set("appVersion", "v0.9.63");
 
   Backbone.history.start({ pushState: true });
   // initTemplateChecks();
