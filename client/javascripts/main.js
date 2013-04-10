24e836796955e33e180da520c80bd2dc057b4d12
Mark Gallop <mark@rollick.id.au>
2013-04-11 05:41:19 +0700

    bump version and tweak country filter margins

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -269,7 +269,7 @@ geoLocation = function(location, inputId, callback) {
 timeout = null;
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.72");
+  Session.set("appVersion", "v0.9.73");
 
   Backbone.history.start({ pushState: true });
   // initTemplateChecks();
