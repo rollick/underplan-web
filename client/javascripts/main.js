50d916e8d4a520bddd05bfefe8a8e495ba85f99d
Mark Gallop <mark@rollick.id.au>
2013-04-19 02:06:17 +0700

    messing around with sass imports to reduce final css file size

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -287,7 +287,7 @@ this.logRenders = function () {
 }
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.83");
+  Session.set("appVersion", "v0.9.84");
 
   // Routing
   Backbone.history.start({ pushState: true });
