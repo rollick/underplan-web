a690962bfff442ecdfdd177d319f3c052400922d
Mark Gallop <mark@rollick.id.au>
2013-04-19 02:36:11 +0700

    update meteor and tweak css for nav

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -287,7 +287,7 @@ this.logRenders = function () {
 }
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.84");
+  Session.set("appVersion", "v0.9.85");
 
   // Routing
   Backbone.history.start({ pushState: true });
