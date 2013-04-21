025a52a331e422dfe1c34779cf978a7ed8cbd0f4
Mark Gallop <mark@rollick.id.au>
2013-04-22 03:57:20 +0700

    lets try appcache again...

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -41,7 +41,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.94");
+  Session.set("appVersion", "v0.9.95");
 
   // Routing
   Backbone.history.start({ pushState: true });
