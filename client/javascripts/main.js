21939e3ca0ec3241e28fc344f347ba5c47282f39
Mark Gallop <mark@rollick.id.au>
2013-04-25 17:51:24 +0700

    add ribbon for stories in feed, and add some rounded / shaded styling to feed and country filter

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -41,7 +41,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.100");
+  Session.set("appVersion", "v0.9.101");
 
   // Routing
   Backbone.history.start({ pushState: true });
