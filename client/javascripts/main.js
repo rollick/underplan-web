019da30227dc3aff74c1fae1c9bd0ec3c5595b65
Mark Gallop <mark@rollick.id.au>
2013-04-30 01:52:03 +0700

    more subtle gray for secondary buttons

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -49,7 +49,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v0.9.111");
+  Session.set("appVersion", "v0.9.112");
 
   // Routing
   Backbone.history.start({ pushState: true });
