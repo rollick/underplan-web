43acf80808764dc2ef050090d0c29261d9bc42b5
Mark Gallop <mark@rollick.id.au>
2013-07-01 00:33:53 +0800

    fix dashboard bg image and tweak layout

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -49,7 +49,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3b3");
+  Session.set("appVersion", "v1.3b4");
 
   // Routing
   Backbone.history.start({ pushState: true });
