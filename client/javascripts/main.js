f6eada0f7a8546b9769f20a57a25dbeb2942dc8d
Mark Gallop <mark@rollick.id.au>
2013-07-13 16:34:28 +0800

    bump version

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -49,7 +49,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3b9");
+  Session.set("appVersion", "v1.3b10");
 
   // Routing
   Backbone.history.start({ pushState: true });
