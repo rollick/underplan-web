c7abcbc6a1562dcd6092a493642dc1567cc7166c
Mark Gallop <mark@rollick.id.au>
2013-07-13 09:48:56 +0800

    add new up/down etc icons

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -49,7 +49,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3b8");
+  Session.set("appVersion", "v1.3b9");
 
   // Routing
   Backbone.history.start({ pushState: true });
