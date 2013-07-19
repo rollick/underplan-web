98f07c0651a2e90edd107cfef819990557e2f6b7
Mark Gallop <mark@rollick.id.au>
2013-07-19 10:13:15 +0200

    fix feed spacing

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -49,7 +49,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3b14");
+  Session.set("appVersion", "v1.3b15");
 
   // Routing
   Backbone.history.start({ pushState: true });
