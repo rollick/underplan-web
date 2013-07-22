19a191b603cfa80ffb4dfb5c4fd11a8140b77c05
Mark Gallop <mark@rollick.id.au>
2013-07-22 04:49:49 +0200

    a bit more width for page

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -43,7 +43,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3b22");
+  Session.set("appVersion", "v1.3b23");
 
   // Routing
   Backbone.history.start({ pushState: true });
