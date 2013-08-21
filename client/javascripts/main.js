65ba44bd1d2b35553131e7bc48781fb3d9f709ec
Mark Gallop <mark@rollick.id.au>
2013-08-21 22:05:37 +0200

    bump version to force redeploy of prod with minified js/css

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -75,7 +75,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3b38");
+  Session.set("appVersion", "v1.3b39");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
