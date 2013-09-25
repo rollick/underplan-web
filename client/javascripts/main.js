d6be4f4a7339af99404366a3dc37887045dc98f6
Mark Gallop <mark@rollick.id.au>
2013-09-25 14:49:47 +0200

    clean up the dashboard layout

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -75,7 +75,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.1");
+  Session.set("appVersion", "v1.3.2");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
