742e912df6d3089af34c2f9d94f87eb5072d6f07
Mark Gallop <mark@rollick.id.au>
2013-09-26 13:05:57 +0200

    remove pointless notice

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -75,7 +75,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.4");
+  Session.set("appVersion", "v1.3.5");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
