99200bc7223981eb666d51ddece272424fcd077f
Mark Gallop <mark@rollick.id.au>
2013-09-28 03:01:38 +0200

    fix layout for group panel date

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -75,7 +75,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.10");
+  Session.set("appVersion", "v1.3.11");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
