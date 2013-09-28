8cb39dde06193b76c0aa47bc32452644ac082563
Mark Gallop <mark@rollick.id.au>
2013-09-28 14:40:22 +0200

    fix labels for activity controls

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -75,7 +75,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.12");
+  Session.set("appVersion", "v1.3.13");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
