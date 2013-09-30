8cba57e7be3ca1244e070ca0197dd927cfd5d497
Mark Gallop <mark@rollick.id.au>
2013-09-30 18:52:11 +0200

    re-add if checks for feed view

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -76,7 +76,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.16");
+  Session.set("appVersion", "v1.3.17");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
