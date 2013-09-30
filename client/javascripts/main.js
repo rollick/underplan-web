287528f666f1a20bcb134875ce23b1ceabf3eb62
Mark Gallop <mark@rollick.id.au>
2013-09-30 18:46:34 +0200

    add version bump script

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -76,7 +76,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.15");
+  Session.set("appVersion", "v1.3.16");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
