47b235a8b967eae019397096c52170a590c3576a
Mark Gallop <mark@rollick.id.au>
2013-10-01 12:45:25 +0200

    add feed gallery

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.21");
+  Session.set("appVersion", "v1.3.22");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
