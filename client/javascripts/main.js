d73b976f99def60c943cef3b5f799aa8a3038fa4
Mark Gallop <mark@rollick.id.au>
2013-10-03 01:03:20 +0200

    use correct field for photo caption

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.27");
+  Session.set("appVersion", "v1.3.28");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
