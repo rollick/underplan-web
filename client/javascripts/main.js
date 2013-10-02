9c98a2e51a1959fe8c67d0b7ff9522a4e9103ab9
Mark Gallop <mark@rollick.id.au>
2013-10-02 10:13:06 +0200

    quich fix for follow icon height

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.26");
+  Session.set("appVersion", "v1.3.27");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
