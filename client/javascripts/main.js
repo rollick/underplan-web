49eb7d5eed5f95b1fddd34b2b21f827d5c259d55
Mark Gallop <mark@rollick.id.au>
2013-10-20 23:35:18 +0300

    add some lien space to group titles on dashboard and fix menu on firefox

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.42");
+  Session.set("appVersion", "v1.3.43");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
