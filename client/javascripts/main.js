2992d59115d660c3198dd8789155531e27cf4ef3
Mark Gallop <mark@rollick.id.au>
2013-10-22 17:18:46 +0200

    update meteor - security fix for node

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.43");
+  Session.set("appVersion", "v1.3.45");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
