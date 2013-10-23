908151642c7dd93a76727e594af2f721d5295860
Mark Gallop <mark@rollick.id.au>
2013-10-23 22:56:35 +0200

    new style for the story header

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.47");
+  Session.set("appVersion", "v1.3.48");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
