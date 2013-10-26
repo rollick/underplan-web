2920ffa7f2baabd41a0380cf47718db09c7a873d
Mark Gallop <mark@rollick.id.au>
2013-10-26 16:03:29 +0200

    fix class/id for location field

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.54");
+  Session.set("appVersion", "v1.3.55");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
