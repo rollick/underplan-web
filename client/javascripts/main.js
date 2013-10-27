b080c24e8d9526aeb48460e90700c35412f1a762
Mark Gallop <mark@rollick.id.au>
2013-10-27 14:56:14 +0100

    add ellipse after long group titles in page header

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.57");
+  Session.set("appVersion", "v1.3.58");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
