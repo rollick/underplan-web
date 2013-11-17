6115d0d32b6de4ac2c337444d7303d38f0c11390
Mark Gallop <mark@rollick.id.au>
2013-11-17 23:01:39 +0100

    fix for going fullscreen from the large highlight story view

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.74");
+  Session.set("appVersion", "v1.3.75");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
