5162815dc9dafd282d215f1d43c3523d616b2ff2
Mark Gallop <mark@rollick.id.au>
2013-11-17 23:40:17 +0100

    try using -webkit-overflow-scrolling for better scrolling on ios devices

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -81,7 +81,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.78");
+  Session.set("appVersion", "v1.3.79");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
