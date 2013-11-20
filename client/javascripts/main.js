e434892c2a0292d61f5ed4ba4844e376be9db571
Mark Gallop <mark@rollick.id.au>
2013-11-20 23:12:55 +0100

    fix z-index which was preventing the login menu from being accessed

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -86,7 +86,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.91");
+  Session.set("appVersion", "v1.3.92");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
