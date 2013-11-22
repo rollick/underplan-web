c15b681bd462632f609bd09877f434460725285a
Mark Gallop <mark@rollick.id.au>
2013-11-22 19:42:41 +0100

    smaller home page quote and logo for mobile

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -87,7 +87,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.105");
+  Session.set("appVersion", "v1.3.106");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
