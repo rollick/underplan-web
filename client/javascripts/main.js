7fffb7579d0e27634c8ad3b6546431da34677720
Mark Gallop <mark@rollick.id.au>
2013-11-22 22:06:04 +0100

    add border below map on home page => show border when mobile doesn't have handle

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -87,7 +87,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.108");
+  Session.set("appVersion", "v1.3.109");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
