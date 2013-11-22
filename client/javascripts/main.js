2bca8e8152cd43e875339a7fe1832ca0fe307ad6
Mark Gallop <mark@rollick.id.au>
2013-11-22 19:20:55 +0100

    add a little rounding to map marker corners

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -87,7 +87,7 @@ Deps.autorun(function () {
 });
 
 Meteor.startup(function () {
-  Session.set("appVersion", "v1.3.104");
+  Session.set("appVersion", "v1.3.105");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
