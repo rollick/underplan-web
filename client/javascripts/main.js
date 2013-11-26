15cf710d62673bc2c69e620dd28a9c2cf9f558b3
Mark Gallop <mark@rollick.id.au>
2013-11-27 00:11:02 +0100

    add some transparency to map controls and fix login menu

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -32,7 +32,7 @@ self.activityCommentStatus = {};
 Meteor.startup(function () {
   logIfDev("===Starting Underplan===");
 
-  Session.set("appVersion", "v1.3.130");
+  Session.set("appVersion", "v1.3.131");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
