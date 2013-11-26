c417c8dbf0efe2f741dfe002c8ddf98f78e13757
Mark Gallop <mark@rollick.id.au>
2013-11-26 21:46:29 +0100

    fix menus after last zurb update

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -32,7 +32,7 @@ self.activityCommentStatus = {};
 Meteor.startup(function () {
   logIfDev("===Starting Underplan===");
 
-  Session.set("appVersion", "v1.3.128");
+  Session.set("appVersion", "v1.3.129");
 
   // Mixpanel tracking
   mixpanel.init(Meteor.settings.public.mixpanelToken);
