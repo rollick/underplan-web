bf81ac8a792fdc6e89bea15a20a7bdceb47b1bae
Mark Gallop <mark@rollick.id.au>
2013-12-05 14:45:17 +0100

    bump version

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -32,7 +32,7 @@ self.activityCommentStatus = {};
 Meteor.startup(function () {
   logIfDev("===Starting Underplan===");
 
-  Session.set("appVersion", "v1.3.136");
+  Session.set("appVersion", "v1.3.137");
   Session.set('mapReady', false);
 
   // Mixpanel tracking
