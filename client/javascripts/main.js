e25c5fc56dc17a94b07bd87fa218ae9aafdf96fd
Mark Gallop <mark@rollick.id.au>
2013-12-18 13:08:53 +0100

    clean up scss file and add pip back to country drop down

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -32,7 +32,7 @@ this.mappingFsm = null;
 Meteor.startup(function () {
   logIfDev("===Starting Underplan===");
 
-  Session.set("appVersion", "v1.3.167");
+  Session.set("appVersion", "v1.3.168");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
