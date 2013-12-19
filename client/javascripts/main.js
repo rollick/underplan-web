4989953c911fa9ea31988eb1ad435080fcad8c17
Mark Gallop <mark@rollick.id.au>
2013-12-19 22:38:32 +0100

    fix grid row layout for map container, add gradient to logo background and fix settings menus to overflow correctly

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -41,7 +41,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.178");
+  Session.set("appVersion", "v1.3.179");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
