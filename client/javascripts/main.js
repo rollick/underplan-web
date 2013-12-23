6ada7ea24040009be26c94eb4a58187e15848d67
Mark Gallop <mark@rollick.id.au>
2013-12-23 05:02:57 +0100

    the sticky footer is back\!

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -41,7 +41,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.195");
+  Session.set("appVersion", "v1.3.196");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
