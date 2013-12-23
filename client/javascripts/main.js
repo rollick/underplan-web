f31bc9951764f724ae5cebcbf20c0e3a088c144a
Mark Gallop <mark@rollick.id.au>
2013-12-23 04:38:57 +0100

    remove test padding and bump version

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -41,7 +41,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.193");
+  Session.set("appVersion", "v1.3.194");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
