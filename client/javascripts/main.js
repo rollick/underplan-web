c3936eef2a307b32a31b55012be01310d536ed61
Mark Gallop <mark@rollick.id.au>
2013-12-28 19:22:58 +0100

    update layout for single item view to emphasise images over text

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -43,7 +43,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.221");
+  Session.set("appVersion", "v1.3.222");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
