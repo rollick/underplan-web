3f01f261f56702050571308f746da8a32656c1c8
Mark Gallop <mark@rollick.id.au>
2013-12-28 19:39:36 +0100

    some color for the profile background

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -43,7 +43,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.222");
+  Session.set("appVersion", "v1.3.223");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
