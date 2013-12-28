4a26eed03403cbe7f7711ce98774d2b5f368d484
Mark Gallop <mark@rollick.id.au>
2013-12-28 13:58:33 +0100

    experimenting with profile layout in feed view

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -43,7 +43,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.218");
+  Session.set("appVersion", "v1.3.219");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
