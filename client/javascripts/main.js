074321e8d90f9ce13696dfa40c7e86f045661fa1
Mark Gallop <mark@rollick.id.au>
2014-01-02 02:22:54 +0100

    fix meteor temp commit

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -48,7 +48,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.252");
+  Session.set("appVersion", "v1.3.253");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
