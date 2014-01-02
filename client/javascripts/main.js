9f844bd4bbf64ba8bedec3e62204f1e1f4134f00
Mark Gallop <mark@rollick.id.au>
2014-01-02 18:44:59 +0100

    fix overflow so that slider frames off-canvas don't get in the way of outside elements

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -48,7 +48,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.3.259");
+  Session.set("appVersion", "v1.3.260");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
