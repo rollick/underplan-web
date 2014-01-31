b3f74e8e44f60986dc7c63de52903cb8cc562181
Mark Gallop <mark@rollick.id.au>
2014-01-31 22:40:38 +0100

    update meteor commit

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -48,7 +48,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.4.7");
+  Session.set("appVersion", "v1.4.8");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
