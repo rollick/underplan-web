03abcf361e54e9b9f1c9d5a5b00646b5b9a18790
Mark Gallop <mark@rollick.id.au>
2014-03-15 23:35:11 +0100

    update meteor / shark

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -36,7 +36,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.4.19");
+  Session.set("appVersion", "v1.4.20");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
