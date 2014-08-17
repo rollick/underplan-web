039f12dc42b66bfddfef5e04ee128f9ebff32514
Mark Gallop <mark@rollick.id.au>
2014-08-17 23:23:14 +0200

    more informative title and description for activities rss feed

--- server/rss.js
+++ server/rss.js
@@ -6,24 +6,29 @@ RssFeed.publish('activities', function(query) {
 
   // feed handler helpers
   // this.cdata, this.setValue, this.addItem
-  _this.setValue('title', _this.cdata('Underplan.io'));
-  _this.setValue('description', _this.cdata('This is a live feed'));
+  _this.setValue('title', _this.cdata('Underplan: ' + group.name));
+  _this.setValue('description', _this.cdata(group.description));
   _this.setValue('link', 'https://underplan.io/' + group.slug);
   _this.setValue('lastBuildDate', new Date());
   _this.setValue('pubDate', new Date());
   _this.setValue('ttl', 1);
   // managingEditor, webMaster, language, docs, generator
 
-  Activities.find({}).forEach(function(activity) {
+  var activityConds = getActivityConditons(group._id);
+  Activities.find(activityConds, {sort: {created: -1}}).forEach(function(activity) {
     var limit = 240,
         preview = activity.text.substring(0, limit);
 
     if(activity.text.length > limit)
       preview += "...";
 
+    var title = activity.title;
+    if (!title && activity.location)
+       title = activity.location;
+
     _this.addItem({
       guid: activity._id,
-      title: activity.title || 'Shorty',
+      title: title,
       description: preview,
       link: 'https://underplan.io/' + group.slug + '/' + activity._id,
       pubDate: activity.created
