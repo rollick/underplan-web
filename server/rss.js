0106555df5bba2d3832df3dc8983dc4995d72dd5
Mark Gallop <mark@rollick.id.au>
2014-08-17 23:50:53 +0200

    add first pass at group comments rss feed

--- server/rss.js
+++ server/rss.js
@@ -34,5 +34,41 @@ RssFeed.publish('activities', function(query) {
       pubDate: activity.created
     });
   });
+});
+
+RssFeed.publish('comments', function(query) {
+  var _this = this,
+      group = Groups.findOne({slug: query.group});
+
+  if (!group) return
+
+  // feed handler helpers
+  // this.cdata, this.setValue, this.addItem
+  _this.setValue('title', _this.cdata('Underplan: ' + group.name));
+  _this.setValue('description', _this.cdata('Comments for: ' + group.name));
+  _this.setValue('link', 'https://underplan.io/' + group.slug);
+  _this.setValue('lastBuildDate', new Date());
+  _this.setValue('pubDate', new Date());
+  _this.setValue('ttl', 1);
+  // managingEditor, webMaster, language, docs, generator
+
+  var activityConds = getActivityConditons(group._id),
+      activityOptions = {fields: {_id: 1}, sort: {created: -1}},
+      activityIds = [];
+
+  Activities.find(activityConds, activityOptions).forEach( function (activity) { 
+    activityIds.push(activity._id);
+  });
+  
+  // only return the comment _id for use in counts
+  Comments.find({activityId: {$in: activityIds}}, {sort: {created: -1}}).forEach(function(comment) {
+    _this.addItem({
+      guid: comment._id,
+      title: displayName(Meteor.users.findOne({_id: comment.owner})),
+      description: comment.comment,
+      link: 'https://underplan.io/' + group.slug + '/' + comment.activityId,
+      pubDate: comment.created
+    });
+  });
 
 });
