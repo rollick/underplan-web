c7f6c124d7a00d2a71ce9b26488862783776a4c1
Mark Gallop <mark@rollick.id.au>
2014-08-18 20:45:22 +0200

    use latest release of mandrill - using bulk email api now

--- server/models/_common.js
+++ server/models/_common.js
@@ -1,18 +1,25 @@
 App.Utils.sendMail = function (templateName, bcc, subject, htmlText) {
   // This uses the API interface for Mandrill
-  emails = _.forEach(bcc, function (email) {
-    Meteor.Mandrill.sendTemplate({
-      key: process.env.MANDRILL_API_KEY,
-      templateSlug: templateName,
-      templateContent: [
-        {
-          name: 'body',
-          content: htmlText
-        }
-      ],
-      globalMergeVars: [],
-      mergeVars: [],
-      toEmail: email
-    });
+  emails = _.map(bcc, function (email) {
+    return {
+      email: email
+    }
+  });
+
+  Meteor.Mandrill.sendTemplate({
+    key: process.env.MANDRILL_API_KEY,
+    template_name: templateName,
+    template_content: [
+      {
+        name: 'body',
+        content: htmlText
+      }
+    ],
+    message: {
+      global_merge_vars: [],
+      merge_vars: [],
+      from_email: 'noreply@underplan.io',
+      to: emails
+    }
   });
 };
