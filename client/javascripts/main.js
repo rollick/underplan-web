c7d024357588bd7a89c5607ef6939d37d82272b1
Mark Gallop <mark@rollick.id.au>
2014-08-18 20:45:35 +0200

    bump release

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -58,7 +58,7 @@ Meteor.startup(function () {
     bodyStyle.insertRule(beforeStyle, bodyStyle.cssRules.length);
   }
 
-  Session.set("appVersion", "v1.4.32");
+  Session.set("appVersion", "v1.4.33");
   Session.set('mapReady', false);
   ReactiveGroupFilter.set("groupSlug", null);
 
