07c5d114fcb574e61ca6b0cd8c278f1910361d16
Mark Gallop <mark@rollick.id.au>
2014-09-21 17:22:30 +0200

    add mixpanel packages which correctly loads underscore

---
+++ packages/meteor-mixpanel/package.js
@@ -0,0 +1,10 @@
+Package.describe({
+	summary: "mixpanel analytics repackaged for Meteor"
+});
+
+Package.on_use(function (api) {
+	api.use(['underscore'], ['client']);
+	api.add_files([
+		'mixpanel-client.js'
+	],'client');
+});
