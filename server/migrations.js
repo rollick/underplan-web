13ccfc9c3309fbf4c3613e9793441c94bfb381cc
Mark Gallop <mark@rollick.id.au>
2014-10-06 22:33:11 +0200

    create migrations for group defaults

--- server/migrations.js
+++ server/migrations.js
@@ -1,5 +1,10 @@
 var migrations = function () {
+  // Set Activity type to story if none exists
   Activities.update({type: {$exists: false}}, {$set: {type: "story"}});
+  
+  // Set Group defaults if none exist
+  Groups.update({defaultView: {$exists: false}}, {$set: {defaultView: "Map"}});
+  Groups.update({hidden: {$exists: false}}, {$set: {hiddne: false}});
 };
 
 Meteor.startup(function() {
